3571b6916b5a25e75bb75510e0bda0f58e9b5a57
Jatin Dhamija <jd.codebase@gmail.com>
2025-06-27 15:04:18 +0530

    meta data

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,12 +6,14 @@ export const metadata = {
   title: "JDCodebase - Tech Simplified",
   description:
     "Explore programming languages, DSA, projects, and blogs to elevate your developer journey.",
+  other: {
+    "google-site-verification": "hQSdP4B_Lwo9VIf3Dp7aqfyVsp5Fi5DZURPpZDMKyT4",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head />
       <body className="bg-gradient-to-br from-neutral-900 via-neutral-800 to-neutral-700 text-white min-h-screen flex flex-col">
         <NavBar />
         <main className="flex-1" role="main">
